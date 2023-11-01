fb75f10b9db6a790dfb492e5ccec84efddc0a30d
ana-ku <user@DESKTOP-1IVSNDL>
2023-11-01 18:11:30 +0100

    Add project files.

---
+++ 4. ukol/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.WriteLine("Zadej text:");
+string radek = Console.ReadLine();
+char[] charArray = radek.ToLower().ToCharArray();
+
+List<char> seznamZnaku = new List<char>();
+List<int> pocetZnaku = new List<int>();
+
+for (int i = 0; i < charArray.Length; i++)
+{
+    
+    if (!seznamZnaku.Contains(charArray[i]))
+    {
+        seznamZnaku.Add(charArray[i]);
+        pocetZnaku.Add(0);
+    }
+    int index = seznamZnaku.IndexOf(charArray[i]);
+    pocetZnaku[index]++;
+}
+
+
+for (int i = 0; i<pocetZnaku.Count; i++)
+{
+    Console.WriteLine(seznamZnaku[i] + "-" + pocetZnaku[i]);
+}
+
+
+
+
